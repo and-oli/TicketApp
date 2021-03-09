9e3323d242a92031510bd26e74b110368e218c8f
Daniel Andres Valencia Ruiz <daniel.valencia@Comsistelco.local>
2021-03-09 16:12:50 -0500

    Implementacion de observaciones

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,83 +1,104 @@
-import React from 'react';
-import './styles/Login.css';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import "./styles/Login.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
+export default function Login() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState({ text: "", color: "green" });
+  const [loading, setloading] = useState(false);
 
+  function userChange(event) {
+    setUserName(event.target.value);
+  }
+  function passwordChange(event) {
+    setPassword(event.target.value);
+  }
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: '',
-            password: '',
-            respuesta: ''
-        };
-
-        this.userChange = this.userChange.bind(this);
-        this.contraseñaChange = this.contraseñaChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!loading) {
+      setloading(true);
+      let user = {
+        name: userName,
+        password: password,
+      };
+      fetch("http://localhost:3000/users/find", {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setloading(false);
+          if (res.ok) {
+            setMessage({ text: res.mensaje, color: "green" });
+          } else {
+            setMessage({ text: res.mensaje, color: "red" });
+          }
+        });
     }
+  }
 
-    userChange(event) {
-        this.setState({ userName: event.target.value });
-    }
-    contraseñaChange(event) {
-        this.setState({ password: event.target.value });
-    }
-
-
-    handleSubmit(event) {
-        event.preventDefault()
-        let user = {
-            userName: this.state.userName,
-            password: this.state.password
-        }
-        console.log(this.state.userName, this.state.password)
-        fetch('http://localhost:3000/users', {
-            method: 'Post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(user)
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({ respuesta: result.mensaje })
-                }
-            )
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="image">
-                    <img className="logo" src="https://comsistelco.com/assets/images/logo_33.png" alt="Logo comsistelco" />
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <div className="login-container">
-                        <h2 className="title" >Iniciar Sesión</h2>
-                        <div>
-                            <form className="container-title" onSubmit={this.handleSubmit}>
-                                <TextField type="text" label="E-mail"
-                                    style={{ width: 200, alignSelf: 'center' }}
-                                    size='small'
-                                    value={this.state.userName}
-                                    onChange={this.userChange}
-                                    required />
-                                <TextField type="password" label="Contraseña"
-                                    style={{ width: 200, alignSelf: 'center' }}
-                                    value={this.state.password}
-                                    onChange={this.contraseñaChange}
-                                    required />
-                                <Button variant="contained" type="submit" color='default' style={{ width: 130, fontSize: 10, alignSelf: 'center', marginTop: 30 }}>
-                                    Ingresar
-                            </Button>
-                            </form>
-                        </div>
-                    </div>
-                    <h1 style={{ position: 'absolute', alignSelf: 'flex-end', top: '80%' }}>{this.state.respuesta}</h1>
-                </div>
-            </div>
-        )
-    }
-};
+  return (
+    <div>
+      <div className="image">
+        <img className="logo" src={process.env.PUBLIC_URL + '/logoComsistelco.jpg'} alt="Logo comsistelco" />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className="login-container">
+          <h2 className="title">Iniciar Sesión</h2>
+          <div>
+            <form className="container-title" onSubmit={handleSubmit}>
+              <TextField
+                type="text"
+                label="E-mail"
+                style={{ width: 200, alignSelf: "center" }}
+                size="small"
+                value={userName}
+                onChange={userChange}
+                required
+              />
+              <TextField
+                type="password"
+                label="Contraseña"
+                style={{ width: 200, alignSelf: "center" }}
+                value={password}
+                onChange={passwordChange}
+                required
+              />
+              <h6 style={{ color: message.color, marginTop: 0 }}>
+                {message.text}
+              </h6>
+              {loading ? (
+                <CircularProgress
+                  style={{ width: 40, alignSelf: "center", marginTop: 30 }}
+                  disableShrink
+                />
+              ) : (
+                <Button
+                  variant="contained"
+                  type="submit"
+                  color="default"
+                  style={{
+                    width: 130,
+                    fontSize: 10,
+                    alignSelf: "center",      
+                    marginTop: 30,
+                  }}
+                >
+                  Ingresar
+                </Button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
