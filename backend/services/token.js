103135df0e0588b85d30d9735db2c8e5cf4b109f
Daniel Andres Valencia Ruiz <daniel.valencia@Comsistelco.local>
2021-03-11 11:25:46 -0500

    implementacion de token a la api de usuario

---
+++ backend/services/token.js
@@ -0,0 +1,80 @@
+const Users = require("../models/users");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../config/config").secret
+
+module.exports = {
+  checkTokenAdmin: function (req, res, next, admin) {
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    if (token) {
+      jwt.verify(token, secretKey, function (err, decoded) {
+        if (err)
+          return res.status(403).send({ success: false, message: "Error de autenticación, por favor refresque la aplicación." });
+        else if (admin && decoded.rol !== "ADMINISTRADOR") {
+          return res.status(403).send({
+            success: false,
+            message: "No tiene permiso para hacer realizar esta acción."
+          });
+        }
+        else {
+          req.decoded = decoded;
+        }
+      });
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "No tiene permiso para realizar esta acción."
+      });
+    }
+    next()
+  },
+
+  checkToken: function (req, res, next) {
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
+    if (token) {
+      jwt.verify(token, secretKey, function (err, decoded) {
+        if (err)
+          return res.status(403).send({ success: false, message: "Error de autenticación, por favor refresque la aplicación." });
+        else {
+          req.decoded = decoded;
+        }
+      });
+    } else {
+      return res.status(403).send({
+        success: false,
+        message: "No tiene permiso para realizar esta acción."
+      });
+    }
+    next()
+  },
+
+  authorizeUser: async function (res, user) {
+    let authorize = await Users.find({ name: user.name })
+      .select("idUser name password role")
+      .then(function (userInfo) {
+        const userAuthorize = userInfo[0]
+        let password = userAuthorize.comparePassword(user.password);
+        if (!password) {
+          res.json({ mensaje: "contraseña incorrecta", ok: false });
+        } else {
+          var token = jwt.sign({
+            id: userAuthorize.idUser,
+            name: userAuthorize.name,
+            userName: userAuthorize.userName,
+            role: userAuthorize.role
+          }, secretKey, { expiresIn: '24h' })
+          res.json({
+            mensaje: "Usuario valido",
+            idUsuario: userAuthorize.idUser,
+            admin: userAuthorize.role === "ADMINISTRADOR",
+            nombre: userAuthorize.name,
+            token,
+            ok: true
+          });
+        }
+      })
+      .catch((err) => {
+        res.json({ mensaje: "Usuario incorrecto", err, ok: false });
+      });
+    return authorize;
+  },
+}
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,47 +1,28 @@
 const Users = require("../models/users");
-const jwt = require("jsonwebtoken");
-module.exports = {
-    postUser: async function (user) {
-        const newUser = new Users()
-        newUser.idUser = user.idUser;
-        newUser.name = user.name;
-        newUser.email = user.email;
-        newUser.password = user.password;
-        newUser.role = user.role;
-        let userRes = (await newUser.save()
-            .then(user => {
-                return { mensaje: "usuario creado correctamente", user, ok: true }
-            })
-            .catch(err => {
-                return { mensaje: "El usuario no pudo ser creado", err, ok: false }
-            }))
-        return userRes
-    },
-    updateUser: async function (user) {
-        let userUp = (await Users.updateOne({ name: user.name }, { password: user.password })
-            .then(user => {
-                return { mensaje: "Usuario actualizado", user, ok: true }
-            })
-            .catch(err => {
-                return { mensaje: "el usuario no pudo ser actualizado", err, ok: false }
-            }))
-        return userUp
-    },
-    authorizeUser: async function (user) {
-        let authorize = (await Users.find({ name: user.name }).select("idUser name password")
-            .then(function (userInfo) {
-                let password = userInfo[0].comparePassword(user.password)
-                if (!password) {
-                    return { mensaje: "contraseña incorrecta", ok: false }
-                } else {
-                    return { mensaje: "Usuario valido", ok: true }
-                }
-            })
-            .catch(err => {
-                return { mensaje: "Usuario insorrecto", err, ok: false }
-            })
-        )
-        return authorize
-    }
 
-}
+module.exports = {
+  postUser: async function (res, user) {
+    const newUser = new Users();
+    newUser.idUser = user.idUser;
+    newUser.name = user.name;
+    newUser.email = user.email;
+    newUser.password = user.password;
+    newUser.role = user.role;
+    let userRes = await newUser
+      .save()
+      .then((user) => res.json({ mensaje: "usuario creado correctamente", user, ok: true }))
+      .catch((err) => res.json({ mensaje: "El usuario no pudo ser creado", ok: false })
+      );
+    return userRes;
+  },
+  updateUser: async function (res, user) {
+    let userUp = await Users.updateOne(
+      { name: user.name },
+      { password: user.password }
+    )
+      .then((user) => res.json({ mensaje: "Usuario actualizado", user, ok: true }))
+      .catch((err) => res.json({ mensaje: "el usuario no pudo ser actualizado", ok: false })
+      );
+    return userUp;
+  }
+};
