9d9751184e8a1dee0b9ef89010f9262a8c855710
Jhonny Alexander Pineda Cuitiva <japineda99@misena.edu.co>
2021-03-11 11:37:20 -0500

    rutas y servicios solicitudes.

--- backend/routes/apiSolicitudes.js
+++ backend/routes/apiSolicitudes.js
@@ -1,77 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-const SolicitudSchema = require('../models/Solicitudes');
-const Solicitudes = mongoose.model('solicitudes', SolicitudSchema);
+const solicitudesService = require('../services/solicitudesService');
 
-function enviarError(error) {
-  res.json({
-    mensaje: 'Hubo un error...',
-    ok: false,
-  });
-  console.error(error);
-};
-
-router.get('/', function (req, res) {
-  Solicitudes.find().then(solicitudes => res.json({
-    mensaje: 'Solicitud exitosa...',
-    ok: true,
-    solicitudes,
-  }))
-    .catch(enviarError);
+router.get('/', async function (req, res) {
+  const rutaGeneral = await solicitudesService.getSolicitudes(res);
+  return rutaGeneral
 });
 
-router.get('/asignadasAUsuario/:idUsuarioMongo', function (req, res) {
-  Solicitudes.find({ idUsuarioMongo: req.params.idUsuarioMongo })
-    .populate('idUsuarioMongo').then(solicitudPorUsuario => res.json(solicitudPorUsuario))
-    .catch(enviarError)
+router.get('/asignadasAUsuario/:idUsuarioMongo', async function (req, res) {
+  const rutaPorUsuario = await solicitudesService.getSoliUsuario(res, req);
+  return rutaPorUsuario
 });
 
-router.get('/porNumero/:idSolicitud', function (req, res) {
-  Solicitudes.find({ idSolicitud: req.params.idSolicitud })
-    .then(solicitudPorId => {
-      if (solicitudPorId.length === 0) {
-        res.json({
-          mensaje: `No se encontraron solicitudes con ese id: ${req.params.idSolicitud}`,
-          ok: false,
-        })
-      } else {
-        res.json({
-          mensaje: 'Solicitud exitosa...',
-          ok: true,
-          solicitud: solicitudPorId[0],
-        })
-      }
-    })
-    .catch(enviarError);
+router.get('/porNumero/:idSolicitud', async function (req, res) {
+  const rutaPorId = await solicitudesService.getSoliNumero(res, req);
+  return rutaPorId
 });
 
-router.post('/', function (req, res) {
-  Solicitudes.create({
-    idSolicitud: req.body.idSolicitud,
-    resumen: req.body.resumen,
-    desripcion: req.body.desripcion,
-    fechaHora: new Date(),
-    estado: req.body.estado,
-    categoria: req.body.categoria,
-    idUsuarioMongo: req.body.idUsuarioMongo,
-    idCliente: req.body.idCliente,
-    // idTarea: { Tarea },
-    // idProyecto: { Proyecto },
-  })
-    .then(solicitudcreada => res.json({
-      mensaje: 'Solicitud creada...',
-      ok: true,
-      solicitud: solicitudcreada,
-    })).catch(enviarError)
+router.post('/', async function (req, res) {
+  const rutaCrear = await solicitudesService.postSolicitud(res, req);
+  return rutaCrear
 });
 
-// ------para cambios solicitud --------------------------------------
-router.put('/:estado', function (req, res) {
-  Solicitudes.findOneAndUpdate({ estado: req.body.estado }
-  ).then(estado => res.json(estado))
-    .catch(error => console.log(error, 'Hubo un error...'))
-});
+// // ------para cambios solicitud --------------------------------------
+// router.put('/:estado', function (req, res) {
+//   Solicitudes.findOneAndUpdate({ estado: req.body.estado }
+//   ).then(estado => res.json(estado))
+//     .catch(error => console.log(error, 'Hubo un error...'))
+// });
 
 module.exports = router;
 
---
+++ backend/services/solicitudesService.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose')
+const SolicitudSchema = require('../models/Solicitudes');
+const Solicitudes = mongoose.model('solicitudes', SolicitudSchema);
+
+function enviarError(error) {
+  console.error(error);
+  return {
+    mensaje: 'Hubo un error...',
+    ok: false,
+  };
+
+};
+
+module.exports = {
+
+  getSolicitudes: async function (res) {
+    const mostrarSolicitudes = (await Solicitudes.find().then(solicitudes => res.json({
+      mensaje: 'Solicitud exitosa...',
+      ok: true,
+      solicitudes,
+    }))
+      .catch(enviarError)
+    )
+    return mostrarSolicitudes;
+  },
+
+  getSoliUsuario: async function (res, req) {
+    const solitudesPorUsuario = (await Solicitudes.find({ idUsuarioMongo: req.params.idUsuarioMongo })
+      .populate('idUsuarioMongo').then(solicitudPorUsuario => res.json({
+        mensaje: 'Solicitud exitosa...',
+        ok: true,
+        solicitudes: solicitudPorUsuario,
+      }))
+      .catch(enviarError)
+    )
+    return solitudesPorUsuario;
+  },
+
+  getSoliNumero: async function (res, req) {
+    const solicitudesPorId = (await Solicitudes.find({ idSolicitud: req.params.idSolicitud })
+      .then(solicitudPorId => {
+        if (solicitudPorId.length === 0) {
+          res.json({
+            mensaje: `No se encontraron solicitudes con ese id: ${req.params.idSolicitud}`,
+            ok: false,
+          })
+        } else {
+          res.json({
+            mensaje: 'Solicitud exitosa...',
+            ok: true,
+            solicitud: solicitudPorId[0],
+          })
+        }
+      })
+      .catch(enviarError)
+    )
+    return solicitudesPorId;
+  },
+
+  postSolicitud: async function (res, req) {
+    const crearSolicitud = (await Solicitudes.create({
+      idSolicitud: req.body.idSolicitud,
+      resumen: req.body.resumen,
+      desripcion: req.body.desripcion,
+      fechaHora: new Date(),
+      estado: req.body.estado,
+      categoria: req.body.categoria,
+      idUsuarioMongo: req.body.idUsuarioMongo,
+      idCliente: req.body.idCliente,
+      // idTarea: { Tarea },
+      // idProyecto: { Proyecto },
+    })
+      .then(solicitudcreada => res.json({
+        mensaje: 'Solicitud creada...',
+        ok: true,
+        solicitud: solicitudcreada,
+      }))
+      .catch(enviarError)
+    )
+    return crearSolicitud;
+  },
+
+};
