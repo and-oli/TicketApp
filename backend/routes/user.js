61079d6628898b85817edf8e4c06986e106412c5
Jhonny Alexander Pineda Cuitiva <japineda99@misena.edu.co>
2021-03-17 16:41:27 -0500

    Actualizaciones de usuario

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,28 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const token = require("../services/token_service");
+const token = require('../services/token_service');
 const userService = require('../services/usuario_service')
 
 router.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type, Authorization,Accept,x-access-token"
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type, Authorization,Accept,x-access-token'
   );
   next();
 });
 
-router.post("/",  async function (req, res) {
-  return await userService.postUser(res, req.body);
+router.post('/', async function (req, res) {
+  return await userService.postUser(req.body, res);
 });
 
-router.post("/editar", token.checkToken, async function (req, res) {
-  return await userService.updateUser(res, req.body);
-});
+router.post('/editar/password', token.checkToken, (req, res, next) => userService
+  .validUser(req.body, res, next), async function (req, res) {
+    return await userService.updatePassword(req.body, res);
+  });
 
-router.post("/authenticate", async function (req, res) {
-  return await token.authorizeUser(res, req.body);
+router.post('/editar/email', token.checkToken, (req, res, next) => userService
+  .validUser(req.body, res, next), async function (req, res) {
+    return await userService.updateEmail(req.body, res);
+  });
+
+router.post('/editar/username', token.checkToken, (req, res, next) => userService
+  .validUser(req.body, res, next), async function (req, res) {
+    return await userService.updateUsername(req.body, res);
+  });
+
+router.post('/authenticate', async function (req, res) {
+  return await token.authorizeUser(req.body, res);
 });
 
 module.exports = router;
