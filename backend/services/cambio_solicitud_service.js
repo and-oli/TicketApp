bd3408a3ced84cd4f214752b5e4bf0b1222249cb
Daniel Andres Valencia Ruiz <daniel.valencia@Comsistelco.local>
2021-03-23 09:41:47 -0500

    servicio envio de correos

--- backend/services/cambio_solicitud_service.js
+++ backend/services/cambio_solicitud_service.js
@@ -10,14 +10,14 @@ const fetch = require('node-fetch');
 module.exports = {
   cambio: async function (req, res) {
 
-    const cambios = req.body
+    const cambios = req.body;
 
     const resultadoSolicitud = {};
     if (cambios.abierta !== undefined) {
-      resultadoSolicitud.abierta = cambios.abierta
+      resultadoSolicitud.abierta = cambios.abierta;
     };
     if (cambios.estado) {
-      resultadoSolicitud.estado = cambios.estado
+      resultadoSolicitud.estado = cambios.estado;
     };
 
     await Solicitud.updateOne({ _id: req.params.idSolicitud }, resultadoSolicitud);
@@ -32,51 +32,53 @@ module.exports = {
     await this.enviarCorreo(req, resultadoSolicitud, cambios.nota);
 
     try {
-      await cambiosSolicitud.save()
+      await cambiosSolicitud.save();
       res.json({
         mensaje: 'Cambios guardado.',
         ok: true,
-      })
+      });
     } catch (err) {
       console.error(err);
       res.json({
         mensaje: 'Hubo un error.',
         ok: false,
-      })
-    }
+      });
+    };
   },
 
   enviarCorreo: async function (req, cambios, nota) {
     let incumbentes = [];
 
-    const solicitud = await Solicitud.findOne({ _id: req.params.idSolicitud })
-      .select('listaIncumbentes');
-    incumbentes = solicitud.listaIncumbentes;
+    try {
+      const solicitud = await Solicitud.findOne({ _id: req.params.idSolicitud })
+        .select('listaIncumbentes');
+      incumbentes = solicitud.listaIncumbentes;
 
-    const emailUsuarios = await Usuario.find({ _id: incumbentes }).select('email');
-    const emails = emailUsuarios.map(usuario => usuario.email);
-    const { emailjsUserId, emailjsTemplateId, emailjsServiceId } = credencialesDeCorreo
+      const emailUsuarios = await Usuario.find({ _id: incumbentes }).select('email');
+      const emails = emailUsuarios.map(usuario => usuario.email);
+      const { emailjsUserId, emailjsTemplateId, emailjsServiceId } = credencialesDeCorreo
 
-    data = {
-      service_id: emailjsServiceId,
-      template_id: emailjsTemplateId,
-      user_id: emailjsUserId,
-      template_params: {
-        'email': emails[0],
-        'title': 'Hubo un cambio en la solicitud:',
-        'nota': nota,
-        'cambios': Object.values(cambios),
-        'bcc': emails
-      }
+      data = {
+        service_id: emailjsServiceId,
+        template_id: emailjsTemplateId,
+        user_id: emailjsUserId,
+        template_params: {
+          'email': emails[0],
+          'title': 'Hubo un cambio en la solicitud:',
+          'nota': nota,
+          'cambios': Object.values(cambios),
+          'bcc': emails
+        }
+      };
 
+      await fetch('https://api.emailjs.com/api/v1.0/email/send', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+        contentType: 'application/json'
+      });
+    } catch (error) {
+      console.log(error);
     }
-    fetch('https://api.emailjs.com/api/v1.0/email/send', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
-      contentType: 'application/json'
-    })
-      .catch(error =>
-        console.error(error.stack))
   },
 }
